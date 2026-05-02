e5f6f9a7e3735c8e01108e9054981a8c05fd46e6
Riadul Islam <riadul551@gmail.com>
2026-05-02 12:54:23 +0600

    initialize the project

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Roboto } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const roboto = Roboto({
+  weight: "400",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata = {
@@ -18,11 +14,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" data-theme="light" className={roboto.className}>
+      <body className="min-h-screen flex flex-col">{children}</body>
     </html>
   );
 }
