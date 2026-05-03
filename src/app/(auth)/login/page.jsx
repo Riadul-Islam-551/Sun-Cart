7e6f7f0d15f276c8c4e2345bfd41b8fa93bef6b1
Riadul Islam <riadul551@gmail.com>
2026-05-03 17:15:38 +0600

    Implement LoginPage with form validation and add logo assets

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,9 +1,84 @@
+"use client";
+
+import {
+  Button,
+  Description,
+  FieldError,
+  Form,
+  Input,
+  Label,
+  TextField,
+} from "@heroui/react";
+import Image from "next/image";
 import React from "react";
+import { FaCheck } from "react-icons/fa";
+import loginLogo from "../../../assets/login-logo.png";
 
 const LoginPage = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="flex justify-center items-center h-[80vh] bg-base-200 ">
+      <Form
+        className="flex w-100 flex-col gap-4 border border-gray-300 p-4 rounded-2xl shadow-lg bg-transparent "
+        render={(props) => <form {...props} data-custom="foo" />}
+        // onSubmit={onSubmit}
+      >
+        <Image
+          src={loginLogo}
+          alt="Login Logo"
+          width={600}
+          height={600}
+          className="w-10/12 mx-auto rounded-xl mb-12  "
+        />
+        <TextField
+          isRequired
+          name="email"
+          type="email"
+          validate={(value) => {
+            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+              return "Please enter a valid email address";
+            }
+            return null;
+          }}
+        >
+          <Label>Email</Label>
+          <Input placeholder="Enter your email" />
+          <FieldError />
+        </TextField>
+        <TextField
+          isRequired
+          minLength={8}
+          name="password"
+          type="password"
+          validate={(value) => {
+            if (value.length < 8) {
+              return "Password must be at least 8 characters";
+            }
+            if (!/[A-Z]/.test(value)) {
+              return "Password must contain at least one uppercase letter";
+            }
+            if (!/[0-9]/.test(value)) {
+              return "Password must contain at least one number";
+            }
+            return null;
+          }}
+        >
+          <Label>Password</Label>
+          <Input placeholder="Enter your password" />
+          <Description>
+            Must be at least 8 characters with 1 uppercase and 1 number
+          </Description>
+          <FieldError />
+        </TextField>
+        <div className="flex gap-2">
+          <Button type="submit">
+            <FaCheck />
+            Submit
+          </Button>
+          <Button type="reset" variant="secondary">
+            Reset
+          </Button>
+        </div>
+      </Form>
     </div>
   );
 };
