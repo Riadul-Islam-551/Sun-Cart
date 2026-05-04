e3bcf68583c905cf3c13ac14f24070f58710caa9
Riadul Islam <riadul551@gmail.com>
2026-05-04 17:18:24 +0600

    Refactor registration page layout and enhance form structure; improve styling and accessibility

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -61,95 +61,115 @@ const RegisterPage = () => {
   };
 
   return (
-    <div className="flex justify-center items-center min-h-[80vh] bg-base-200 ">
-      <Form
-        className="flex w-100 flex-col gap-4 border border-gray-300 p-4 rounded-2xl shadow-lg bg-transparent mx-2 my-12 "
-        render={(props) => <form {...props} data-custom="foo" />}
-        onSubmit={handleRegistrationSubmit}
-      >
-        <Image
-          src={registrationLogo}
-          alt="Registration"
-          width={600}
-          height={600}
-          className="w-8/12 mx-auto rounded-xl mb-12  "
-        />
-        {/* name field  */}
-        <TextField
-          isRequired
-          name="name"
-          type="text"
-          validate={(value) => {
-            if (value.length < 3) {
-              return "Name must be at least 3 characters";
-            }
-            return null;
-          }}
+    <div className="flex justify-center items-center flex-col min-h-[80vh] bg-base-200">
+      <div className=" border border-gray-300 p-4 rounded-2xl shadow-lg bg-transparent mx-2 my-12 ">
+        <Form
+          className="flex w-100 flex-col gap-4  "
+          render={(props) => <form {...props} data-custom="foo" />}
+          onSubmit={handleRegistrationSubmit}
         >
-          <Label>Name</Label>
-          <Input placeholder="Enter your name" />
-          <FieldError />
-        </TextField>
-        {/* image url field  */}
-        <TextField isRequired name="image" type="text">
-          <Label>Image Url</Label>
-          <Input placeholder="Enter your image url" />
-          <FieldError />
-        </TextField>
-        {/* email field  */}
-        <TextField
-          isRequired
-          name="email"
-          type="email"
-          validate={(value) => {
-            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
-              return "Please enter a valid email address";
-            }
-            return null;
-          }}
-        >
-          <Label>Email</Label>
-          <Input placeholder="Enter your email" />
-          <FieldError />
-        </TextField>
-        {/* password field  */}
-        <TextField
-          isRequired
-          minLength={8}
-          name="password"
-          type={showPassword ? "text" : "password"}
-          className="relative"
-          validate={(value) => {
-            if (value.length < 8) {
-              return "Password must be at least 8 characters";
-            }
-            if (!/[A-Z]/.test(value)) {
-              return "Password must contain at least one uppercase letter";
-            }
-            if (!/[0-9]/.test(value)) {
-              return "Password must contain at least one number";
-            }
-            return null;
-          }}
-        >
-          <Label>Password</Label>
-          <Input placeholder="Enter your password" />
-          <Description>
-            Must be at least 8 characters with 1 uppercase and 1 number
-          </Description>
-          <FieldError />
-          {/* show the password or hide the password  */}
-          <span
-            onClick={() => setShowPassword(!showPassword)}
-            className="absolute right-2 top-8.5  cursor-pointer text-gray-500"
+          <Image
+            src={registrationLogo}
+            alt="Registration"
+            width={600}
+            height={600}
+            className="w-8/12 mx-auto rounded-xl mb-12  "
+          />
+          {/* name field  */}
+          <TextField
+            isRequired
+            name="name"
+            type="text"
+            validate={(value) => {
+              if (value.length < 3) {
+                return "Name must be at least 3 characters";
+              }
+              return null;
+            }}
           >
-            {showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
-          </span>
-        </TextField>
+            <Label>Name</Label>
+            <Input placeholder="Enter your name" />
+            <FieldError />
+          </TextField>
+          {/* image url field  */}
+          <TextField isRequired name="image" type="text">
+            <Label>Image Url</Label>
+            <Input placeholder="Enter your image url" />
+            <FieldError />
+          </TextField>
+          {/* email field  */}
+          <TextField
+            isRequired
+            name="email"
+            type="email"
+            validate={(value) => {
+              if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+                return "Please enter a valid email address";
+              }
+              return null;
+            }}
+          >
+            <Label>Email</Label>
+            <Input placeholder="Enter your email" />
+            <FieldError />
+          </TextField>
+          {/* password field  */}
+          <TextField
+            isRequired
+            minLength={8}
+            name="password"
+            type={showPassword ? "text" : "password"}
+            className="relative"
+            validate={(value) => {
+              if (value.length < 8) {
+                return "Password must be at least 8 characters";
+              }
+              if (!/[A-Z]/.test(value)) {
+                return "Password must contain at least one uppercase letter";
+              }
+              if (!/[0-9]/.test(value)) {
+                return "Password must contain at least one number";
+              }
+              return null;
+            }}
+          >
+            <Label>Password</Label>
+            <Input placeholder="Enter your password" />
+            <Description>
+              Must be at least 8 characters with 1 uppercase and 1 number
+            </Description>
+            <FieldError />
+            {/* show the password or hide the password  */}
+            <span
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute right-2 top-8.5  cursor-pointer text-gray-500"
+            >
+              {showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
+            </span>
+          </TextField>
 
+          {/* submit button  */}
+          <div className="flex gap-2">
+            <Button type="submit">
+              <FaCheck />
+              Submit
+            </Button>
+            <Button type="reset" variant="secondary">
+              Reset
+            </Button>
+          </div>
+          <div>
+            <p className="text-center">
+              Do you have an account?{" "}
+              <a href="/login" className="text-red-500 text-sm">
+                Login
+              </a>
+            </p>
+          </div>
+        </Form>
         {/* google login button   */}
         <button
-          className="btn bg-white text-black border-[#e5e5e5] "
+          className="btn bg-white text-black border-[#e5e5e5] w-full mt-4"
           onClick={handleGoogleSignin}
         >
           <svg
@@ -181,27 +201,8 @@ const RegisterPage = () => {
           </svg>
           Login with Google
         </button>
-
-        {/* submit button  */}
-        <div className="flex gap-2">
-          <Button type="submit">
-            <FaCheck />
-            Submit
-          </Button>
-          <Button type="reset" variant="secondary">
-            Reset
-          </Button>
-        </div>
-        <div>
-          <p className="text-center">
-            Do you have an account?{" "}
-            <a href="/login" className="text-red-500 text-sm">
-              Login
-            </a>
-          </p>
-        </div>
-      </Form>
-      <ToastContainer />
+        <ToastContainer />
+      </div>
     </div>
   );
 };
